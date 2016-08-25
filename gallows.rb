4d3f0709f59a84648554a74f3d3869964807f690
Vadim Borisov <borissowadim@mail.ru>
2016-08-26 00:17:17 +0300

    initialize

---
+++ gallows.rb
@@ -0,0 +1,20 @@
+﻿require "./game.rb"
+require "./result_printer.rb"
+
+printer = ResultPrinter.new
+
+slovo = ARGV[0]
+
+if (Gem.win_platform? && ARGV[0])
+		slovo = slovo.encode(ARGV[0].encoding).encode("UTF-8")
+end
+
+game = Game.new(slovo)
+
+while game.status == 0 do
+	printer.print_status(game)
+
+	game.ask_next_letter
+end
+
+printer.print_status(game)
